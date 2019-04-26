619d36e6b9601b067461c281f9c40fa6c8c33768
Fabio L. Janiszevski <contato@fabiosammy.com>
2019-04-26 19:02:11 -0300

    Atualizado com o formato de threading

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy
 import random
 import time
 import logging
+import threading
+import Queue
 
 def cria_matriz(linhas, colunas):
   A = []
@@ -15,11 +17,14 @@ def cria_matriz(linhas, colunas):
     A = A + [linha]
   return A
 
-def multiplica_linha_coluna(matrizA, matrizB, i, j):
-    valor = 0
-    for k in range(len(matrizB)):
-        valor = valor + matrizA[i][k] * matrizB[k][j]
-    return valor
+def multiplica_linha_coluna(queue, queue_resultados, matrizA, matrizB):
+    while True:
+        i, j = queue.get()
+        valor = 0
+        for k in range(len(matrizB)):
+            valor = valor + matrizA[i][k] * matrizB[k][j]
+        queue.task_done()
+        queue_resultados.put((i, j, valor))
 
 if __name__ == '__main__':
     linhas, colunas = 400, 400
@@ -30,8 +35,20 @@ if __name__ == '__main__':
     matrizC = numpy.zeros(shape=(linhas,colunas))
 
     print("{}: Multiplicando matrizes".format(time.strftime('%c')))
+    queue = Queue.Queue()
+    queue_resultados = Queue.Queue()
+    for i in range(2):
+        worker = threading.Thread(target=multiplica_linha_coluna, args=(queue, queue_resultados, matrizA, matrizB,))
+        worker.setDaemon(True)
+        worker.start()
+
     for i in range(len(matrizA)):
         for j in range(len(matrizA[0])):
-            matrizC[i][j] = multiplica_linha_coluna(matrizA, matrizB, i, j)
+            queue.put((i, j))
+
+    while not queue_resultados.empty():
+        i, j, valor = queue_resultados.get()
+        matrizC[i][j] = valor
+        queue_resultados.task_done()
 
     print("{}: Resultado:{}".format(time.strftime('%c'), matrizC))
